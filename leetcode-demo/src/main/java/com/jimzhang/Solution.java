882ae0ca31e379f776acceaec019541a917f6d71
itzjm <ZJM528el@163.com>
2020-06-30 11:19:01 +0800

    add 3 sum

--- leetcode-demo/src/main/java/com/jimzhang/Solution.java
+++ leetcode-demo/src/main/java/com/jimzhang/Solution.java
@@ -1,9 +1,6 @@
 package com.jimzhang;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Stack;
+import java.util.*;
 
 /**
  * @className : Solution
@@ -42,6 +39,66 @@ public class Solution {
         throw new IllegalArgumentException("No two sum solution");
     }
 
+
+    /**
+     * 三数之和：数组遍历
+     * 首先对数组进行排序，排序后固定一个数 nums[i]，再使用左右指针指向 nums[i]后面的两端，数字分别为 nums[L] 和 nums[R]，计算三个数的和 sum 判断是否满足为 0，满足则添加进结果集
+     * 如果 nums[i]大于 0，则三数之和必然无法等于 0，结束循环
+     * 如果 nums[i] == nums[i-1]，则说明该数字重复，会导致结果重复，所以应该跳过
+     * 当 sum == 0 时，nums[L] == nums[L+1] 则会导致结果重复，应该跳过，L++
+     * 当 sum == 0 时，nums[R] == nums[R-1] 则会导致结果重复，应该跳过，R--
+     * 时间复杂度：O(n^2)，n 为数组长度
+     *
+     * 作者：guanpengchn
+     * 链接：https://leetcode-cn.com/problems/3sum/solution/hua-jie-suan-fa-15-san-shu-zhi-he-by-guanpengchn/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     * @param nums
+     * @return
+     */
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        int length = nums.length;
+        if (nums == null || length < 3) {
+            return ans;
+        }
+        // 从小到大排序
+        Arrays.sort(nums);
+        for (int i=0; i<length; i++) {
+            if (nums[i] > 0) {
+                // 当前数字>0，则3数之和一定>0，循环结束
+                break;
+            }
+            if (i > 0 && nums[i] == nums[i-1]) {
+                // 数字重复
+                continue;
+            }
+            // 定义2个指针
+            int L = i+1;
+            int R = nums.length - 1;
+            while (L < R) {
+                int sum = nums[i] + nums[L] + nums[R];
+                if (sum == 0) {
+                    ans.add(Arrays.asList(nums[i],nums[L],nums[R]));
+                    while (L < R && nums[L] == nums[L+1]) {
+                        // 去重
+                        L++;
+                    }
+                    while (L<R && nums[R] == nums[R-1]) {
+                        // 去重
+                        R--;
+                    }
+                    L++;
+                    R--;
+                }else if (sum < 0) {
+                    L++;
+                }else if (sum > 0) {
+                    R--;
+                }
+            }
+        }
+        return ans;
+    }
 
     /**
      * 定义单链表
@@ -98,8 +155,20 @@ public class Solution {
 
 
     /**
-     * 242. 有效的字母异位词
+     * 242. 有效的字母异位词：数组
      * a 在ASCII 中是97，减去 a 就是从数组第一个下标位开始
+     *
+     * 思路：
+     * 标签：哈希映射
+     * 首先判断两个字符串长度是否相等，不相等则直接返回 false
+     * 若相等，则初始化 26 个字母哈希表，遍历字符串 s 和 t
+     * s 负责在对应位置增加，t 负责在对应位置减少
+     * 如果哈希表的值都为 0，则二者是字母异位词
+     *
+     * 作者：guanpengchn
+     * 链接：https://leetcode-cn.com/problems/valid-anagram/solution/hua-jie-suan-fa-242-you-xiao-de-zi-mu-yi-wei-ci-by/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
      * @param s
      * @param t
      * @return
@@ -155,4 +224,6 @@ public class Solution {
         }
         return stack.isEmpty();
     }
+
+
 }
