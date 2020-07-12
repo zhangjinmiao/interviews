ca98b334fc1fc418424ea0267a84632bfb539431
zhangjm <itzhangjm@gmail.com>
2020-07-12 20:19:53 +0800

    add isValidBST‘

--- leetcode-demo/src/main/java/com/jimzhang/Solution.java
+++ leetcode-demo/src/main/java/com/jimzhang/Solution.java
@@ -1,5 +1,7 @@
 package com.jimzhang;
 
+import com.jimzhang.tree.BinaryTreeDemo;
+
 import java.util.*;
 
 /**
@@ -45,13 +47,14 @@ public class Solution {
     /**
      * tag：数组
      *
-     * 三数之和：数组遍历
+     * 三数之和：数组遍历，sort && find 两边往中间夹
      * 首先对数组进行排序，排序后固定一个数 nums[i]，再使用左右指针指向 nums[i]后面的两端，数字分别为 nums[L] 和 nums[R]，计算三个数的和 sum 判断是否满足为 0，满足则添加进结果集
      * 如果 nums[i]大于 0，则三数之和必然无法等于 0，结束循环
      * 如果 nums[i] == nums[i-1]，则说明该数字重复，会导致结果重复，所以应该跳过
      * 当 sum == 0 时，nums[L] == nums[L+1] 则会导致结果重复，应该跳过，L++
      * 当 sum == 0 时，nums[R] == nums[R-1] 则会导致结果重复，应该跳过，R--
      * 时间复杂度：O(n^2)，n 为数组长度
+     * 空间复杂度：O(1)
      *
      * 作者：guanpengchn
      * 链接：https://leetcode-cn.com/problems/3sum/solution/hua-jie-suan-fa-15-san-shu-zhi-he-by-guanpengchn/
@@ -66,7 +69,7 @@ public class Solution {
         if (nums == null || length < 3) {
             return ans;
         }
-        // 从小到大排序
+        // 从小到大排序 快排：O(nlogn)
         Arrays.sort(nums);
         for (int i=0; i<length; i++) {
             if (nums[i] > 0) {
@@ -304,5 +307,29 @@ public class Solution {
         return p2;
     }
 
-    
+    public static class TreeNode {
+        int val;
+        BinaryTreeDemo.TreeNode left;
+        BinaryTreeDemo.TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+    /**
+     * 验证二叉搜索树：
+     *
+     * 二叉搜索树:
+     *  1.节点的左子树只包含小于当前节点的数
+     *  2.节点的右子树只包含大于当前节点的数
+     *  3.所有左子树和右子树自身必须也是二叉搜索树
+     * 时间复杂度：O()
+     * 空间复杂度：O()
+     *
+     * @param root
+     * @return
+     */
+    public boolean isValidBST(TreeNode root) {
+
+        return true;
+    }
+
 }
--- leetcode-demo/src/main/java/com/jimzhang/tree/BinaryTreeDemo.java
+++ leetcode-demo/src/main/java/com/jimzhang/tree/BinaryTreeDemo.java
@@ -1,4 +1,128 @@
 package com.jimzhang.tree;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 二叉树
+ */
 public class BinaryTreeDemo {
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+
+    static ArrayList<Integer> list = new ArrayList<>();
+
+
+    /**
+     * 递归
+     * 前序遍历：中 → 左 → 右
+     * @param root
+     * @return
+     */
+    public static List<Integer> preOrderTraversal(TreeNode root) {
+        if (root == null) {
+            return list;
+        }
+        // 中
+        list.add(root.val);
+        // 左
+        if (root.left != null) {
+            preOrderTraversal(root.left);
+        }
+        // 右
+        if (root.right != null) {
+            preOrderTraversal(root.right);
+        }
+        return list;
+    }
+
+
+
+    /**
+     * 递归
+     * 中序遍历：左 → 中 → 右
+     * @param root
+     * @return
+     */
+    public static List<Integer> inOrderTraversal(TreeNode root) {
+        if (root == null) {
+            return list;
+        }
+        // 左
+        if (root.left != null) {
+            inOrderTraversal(root.left);
+        }
+        // 中
+        list.add(root.val);
+        // 右
+        if (root.right != null) {
+            inOrderTraversal(root.right);
+        }
+        return list;
+    }
+
+    /**
+     * 递归
+     * 后序遍历：左 → 右 → 中
+     * @param root
+     * @return
+     */
+    public static List<Integer> postOrderTraversal(TreeNode root) {
+        if (root == null) {
+            return list;
+        }
+        // 左
+        if (root.left != null) {
+            postOrderTraversal(root.left);
+        }
+        // 右
+        if (root.right != null) {
+            postOrderTraversal(root.right);
+        }
+        // 中
+        list.add(root.val);
+        return list;
+    }
+
+
+    public static void main(String[] args) {
+        /**
+         *        1
+         *    2      6
+         *  3  5    7 8
+         *   4
+         */
+        TreeNode root = new TreeNode(1);
+        TreeNode left = new TreeNode(2);
+        TreeNode right = new TreeNode(6);
+
+        root.left = left;
+        root.right = right;
+
+        TreeNode ll = new TreeNode(3);
+        TreeNode lr = new TreeNode(5);
+
+        left.left = ll;
+        left.right = lr;
+
+        TreeNode llr = new TreeNode(4);
+        ll.right = llr;
+
+        right.left = new TreeNode(7);
+        right.right = new TreeNode(8);
+
+
+//        preOrderTraversal(root); // 1 2 3 4 5 6 7 8
+//          inOrderTraversal(root);  // 3 4 2 5 1 7 6 8
+        postOrderTraversal(root);  // 4 3 5 2 7 8 6 1
+        for (Integer i : list) {
+            System.out.print(i + " ");
+        }
+    }
 }
