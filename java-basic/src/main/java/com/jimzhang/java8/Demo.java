0f60db59ba18bc1eac2b5e68ad59e5eddf3b092b
zhangjm <itzhangjm@gmail.com>
2020-09-07 10:55:41 +0800

    test

--- java-basic/src/main/java/com/jimzhang/java8/Demo.java
+++ java-basic/src/main/java/com/jimzhang/java8/Demo.java
@@ -1,4 +1,14 @@
 package com.jimzhang.java8;
 
+import java.time.LocalDate;
+import java.util.stream.Stream;
+
+import static java.util.stream.Stream.*;
+
 public class Demo {
+
+
+    public static void main(String[] args) {
+        iterate(LocalDate.now(), date -> date.plusDays(1)).limit(1).forEach(System.out::println);
+    }
 }
